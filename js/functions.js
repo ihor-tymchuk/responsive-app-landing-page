38d19e19624fb247500d00df13d205c0f1e85db1
WvDvUglJ <@WvDvUglJ>
2021-03-25 12:50:29 +0200

    fix 3

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,16 @@
 /********* Custom JS functions from EGrappler.com **************/
 
-$(document).ready(function() {
-
-	$('.price-table-toggle').click(function () {
-	
-		var thisparent = $(this).parents('.pricing-table');
-		$('.price-table-features', thisparent).fadeToggle('slow');
-		
-		if($(this).html()=='+ Show features')
-			$(this).html('- Hide features');	
-		else {	
-			$(this).html('+ Show features');	
-		}
-	});
-	
-}); 
-
-
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
